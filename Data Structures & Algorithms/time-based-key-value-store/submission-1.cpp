fc9e11dc859b59c69a503e2272faf12c7a58fd2d
SachetK <71898487+SachetK@users.noreply.github.com>
2026-09-08 12:19:52 -0400

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,27 @@
+class TimeMap {
+public:
+    std::unordered_map<string, std::map<int, string>> store;
+
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        store[key][timestamp] = value;
+    }
+    
+    string get(string key, int timestamp) {
+        auto history = store.find(key);
+        if (history == store.end()) {
+            return "";
+        }
+
+
+        auto&& more = history->second.upper_bound(timestamp); // first element greater than timestamp
+        if (more == history->second.begin()) {
+            return "";
+        }
+
+        return std::prev(more)->second;
+    }
+};
